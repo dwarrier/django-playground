a3bf995204633e294f27b4f2a8b1846a0c409451
Deepak Warrier <dwarrier@gmail.com>
2018-03-25 09:05:39 -0700

    test navbar

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 
 from django.urls import include, path
 from django.contrib import admin
+from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
+    path('', views.redirect_action, name='redirect'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
